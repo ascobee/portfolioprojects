6fa470551e4d1f179c19f3d31ff2bc3d751cb1b3
ascobee <austin.scobee@yahoo.com>
2020-05-11 17:13:35 -0600

    Added documentation to calculator.py. Created alternative code for GUI Calculator.

--- calculator.py
+++ calculator.py
@@ -1,12 +1,23 @@
-# Project: GUI Calculator
-# This calculator can perform addition, subtraction, multiplication, division, and more.
-# Terminal Input: python3 calculator.py
+"""GUI Calculator.
 
+This calculator can perform addition, subtraction, multiplication, 
+division, and more.
+
+Example:
+    Open the calculator interface.
+
+        $ python calculator.py
+
+"""
+
+
+from functools import wraps
 from tkinter import *
 from tkinter import ttk
 
 
 def num_btn(n):
+    """Update value displayed on screen when a number is selected."""
     value = calc_display.get()
     if value != "0":
         value += str(n)
@@ -16,50 +27,63 @@ def num_btn(n):
 
 
 def zero_btn():
+    """Call num_btn() with the corresponding button value."""
     return num_btn(0)
 
 
 def one_btn():
+    """Call num_btn() with the corresponding button value."""
     return num_btn(1)
 
 
 def two_btn():
+    """Call num_btn() with the corresponding button value."""
     return num_btn(2)
 
 
 def three_btn():
+    """Call num_btn() with the corresponding button value."""
     return num_btn(3)
 
 
 def four_btn():
+    """Call num_btn() with the corresponding button value."""
     return num_btn(4)
 
 
 def five_btn():
+    """Call num_btn() with the corresponding button value."""
     return num_btn(5)
 
 
 def six_btn():
+    """Call num_btn() with the corresponding button value."""
     return num_btn(6)
 
 
 def seven_btn():
+    """Call num_btn() with the corresponding button value."""
     return num_btn(7)
 
 
 def eight_btn():
+    """Call num_btn() with the corresponding button value."""
     return num_btn(8)
 
 
 def nine_btn():
+    """Call num_btn() with the corresponding button value."""
     return num_btn(9)
 
 
 def clear_btn():
+    """Reset value displayed on screen to zero."""
     return calc_display.set(0)
 
 
 def value_updater(fn):
+    """Decorator to update value displayed on screen."""
+    @wraps(fn)
     def wrapper(*args, **kwargs):
         value = calc_display.get()
         new_val = fn(value)
@@ -69,6 +93,7 @@ def value_updater(fn):
 
 @value_updater
 def decimal_btn(value):
+    """Add a decimal point to current value."""
     if value.count(".") == 0:
         value += "."
     elif value.count(".") == 1 and value.count(" "):
@@ -78,6 +103,7 @@ def decimal_btn(value):
 
 @value_updater
 def del_btn(value):
+    """Delete the last character from current value."""
     if len(value) > 1:
         if value[-1] == " ":
             return value[0:-3]
@@ -89,6 +115,7 @@ def del_btn(value):
 
 @value_updater
 def neg_btn(value):
+    """Return the current value multiplied by -1."""
     if value.count(" "):
         val_list = value.split()
         num1 = val_list[0]
@@ -104,6 +131,7 @@ def neg_btn(value):
 
 @value_updater
 def reciprocal_btn(value):
+    """Return the reciprocal of the current value."""
     try:
         x = float(value)
         if x != 0:
@@ -116,6 +144,7 @@ def reciprocal_btn(value):
 
 @value_updater
 def squared_btn(value):
+    """Return the square of the current value."""
     try:
         x = float(value)
         return x ** 2
@@ -125,6 +154,7 @@ def squared_btn(value):
 
 @value_updater
 def sqrt_btn(value):
+    """Return the square root of the current value."""
     try:
         x = float(value)
         if x > 0:
@@ -137,6 +167,7 @@ def sqrt_btn(value):
 
 @value_updater
 def percent_btn(value):
+    """Convert the current value to a percentage in decimal form."""
     try:
         x = float(value)
         return x / 100
@@ -145,21 +176,25 @@ def percent_btn(value):
 
 
 def addition(num1, num2):
+    """Return the value of num1 plus num2."""
     new_val = num1 + num2
     return calc_display.set(new_val)
 
 
 def subtraction(num1, num2):
+    """Return the value of num1 minus num2."""
     new_val = num1 - num2
     return calc_display.set(new_val)
 
 
 def multiplication(num1, num2):
+    """Return the value of num1 multiplied by num2."""
     new_val = num1 * num2
     return calc_display.set(new_val)
 
 
 def division(num1, num2):
+    """Return the value of num1 divided by num2."""
     if num2 != 0:
         new_val = num1 / num2
     else:
@@ -168,11 +203,14 @@ def division(num1, num2):
 
 
 def exponent(num1, num2):
+    """Return the value of num1 to the power of num2."""
     new_val = num1 ** num2
     return calc_display.set(new_val)
 
 
 def equation_checker(fn):
+    """Decorator to determine the response to a function call."""
+    @wraps(fn)
     def wrapper(*args, **kwargs):
         value = calc_display.get()
         if value.count(" "):
@@ -184,36 +222,42 @@ def equation_checker(fn):
 
 @equation_checker
 def add_btn(value):
+    """Add a plus sign to the current value."""
     new_val = value + " + "
     return calc_display.set(new_val)
 
 
 @equation_checker
 def sub_btn(value):
+    """Add a minus sign to the current value."""
     new_val = value + " - "
     return calc_display.set(new_val)
 
 
 @equation_checker
 def mult_btn(value):
+    """Add a multiplication sign to the current value."""
     new_val = value + " x "
     return calc_display.set(new_val)
 
 
 @equation_checker
 def div_btn(value):
+    """Add a division sign to the current value."""
     new_val = value + " / "
     return calc_display.set(new_val)
 
 
 @equation_checker
 def exp_btn(value):
+    """Add an exponent symbol to the current value."""
     new_val = value + " ^ "
     return calc_display.set(new_val)
 
 
 @equation_checker
 def equal_btn(value):
+    """Return the current value if math operation is not specified."""
     return calc_display.set(value)
 
 
@@ -227,12 +271,14 @@ math_operators = {
 
 
 def math_selector(num1, math_symbol, num2):
+    """Call the function for the indicated math operation."""
     for key in math_operators.keys():
         if key == math_symbol:
             return math_operators[key](num1, num2)
 
 
 def split_equation(value):
+    """Split the equation into num1, the math operation, and num2."""
     val_list = value.split(" ")
     num1 = float(val_list[0])
     math_symbol = val_list[1]
@@ -240,6 +286,7 @@ def split_equation(value):
     return math_selector(num1, math_symbol, num2)
 
 
+"""Set up the window."""
 root = Tk()
 root.title("Calculator")
 
@@ -248,6 +295,7 @@ mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
+"""Create the widgets and place them onscreen."""
 calc_display = StringVar()
 calc_display.set(0)
 
@@ -308,9 +356,13 @@ ttk.Button(mainframe, text=".", command=decimal_btn).grid(
 ttk.Button(mainframe, text="=", command=equal_btn).grid(
     column=4, row=7, sticky=(W, E))
 
+"""Add padding around each widget."""
 for child in mainframe.winfo_children():
     child.grid_configure(padx=1, pady=1)
 
+"""Call the equal_btn function if the Return key is pressed."""
 root.bind('<Return>', equal_btn)
 
-root.mainloop()
+"""Run the application."""
+if __name__ == "__main__":
+    root.mainloop()
